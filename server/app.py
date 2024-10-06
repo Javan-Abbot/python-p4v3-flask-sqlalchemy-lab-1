4dddf405edc7d7a4f0d4926952e82f3c0f558f67
+javan abbot <+>
2024-10-06 11:04:05 +0300

    db init and response

--- server/app.py
+++ server/app.py
@@ -21,6 +21,41 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    
+    if earthquake is None:
+        body = {"message": f"Earthquake {id} not found."}
+        return make_response(body, 404)
+    body = {
+        "id": earthquake.id,
+        "location": earthquake.location,
+        "magnitude": earthquake.magnitude,
+        "year": earthquake.year
+    }
+    return make_response(body, 200)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def test_earthquake(magnitude):
+    earthquake = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    earthquake_list = []
+    for quake in earthquake:
+        earthquake_data = {
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        }
+        earthquake_list.append(earthquake_data)
+        
+    response_body = {
+            "count": len(earthquake_list),
+            "quakes": earthquake_list
+        }
+        
+    response = make_response(response_body, 200)
+    return response
 
 
 if __name__ == '__main__':
